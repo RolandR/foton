98de3029884c702fd6d4d2b704f0a1492d66a79f
Roland Rytz <roland.rytz@gmail.com>
2018-11-09 20:03:28 +0100

    improve gallery css

--- buildJson.py
+++ buildJson.py
@@ -4,6 +4,7 @@ import sys
 import os
 from os import listdir
 from os.path import isfile, join
+from PIL import Image
 
 thumbSize = "300"
 thumbDirectory = "thumbs/"
@@ -18,7 +19,7 @@ with open("figure.html", 'r') as figureFile:
 	figure = figureFile.read()
 
 path = sys.argv[1]
-files = [f for f in listdir(path) if isfile(join(path, f))]
+files = [f for f in sorted(listdir(path)) if isfile(join(path, f))]
 
 os.system("rm -r "+path+thumbDirectory)
 os.system("mkdir "+path+thumbDirectory)
@@ -30,15 +31,20 @@ json += "\timages: [\n"
 outFigures = ""
 
 for index, file in enumerate(files):
-	print("["+str(index+1)+"/"+str(len(files))+"] Converting "+file)
-	cmd = "convert "+path+file+" -resize "+thumbSize+"x"+thumbSize+" "+path+thumbDirectory+file
+	print("["+str(index+1).zfill(len(str(len(files))))+"/"+str(len(files))+"] Converting "+file)
+	cmd = "convert "+path+file+" -auto-orient -quality 80 -strip -interlace Plane -resize "+thumbSize+"x"+thumbSize+" "+path+thumbDirectory+file
 	os.system(cmd);
 	json += "\t\t"+file
 	if index != len(files)-1:
 		json += ","
 	json += "\n"
+
+	im = Image.open(path+thumbDirectory+file)
+	width, height = im.size
 	
 	outFigure = figure.replace("$SRC", path+thumbDirectory+file)
+	outFigure = outFigure.replace("$WIDTH", str(width))
+	outFigure = outFigure.replace("$HEIGHT", str(height))
 	outFigure = outFigure.replace("$ALT", file)
 	outFigures += outFigure
 
