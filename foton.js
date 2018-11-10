4e8cf2853d1a088344cc564641f23d1cc2d5a66e
Roland Rytz <roland.rytz@gmail.com>
2018-11-10 01:21:49 +0100

    add keyboard navigation, allow overlay to be closed

--- foton.js
+++ foton.js
@@ -1,7 +1,31 @@
+var currentlyDisplayed = null;
 
 function showPhoto(element){
 	var imageName = element.dataset.image;
 
+	display(imageName);
+	
+	document.getElementById("overlay").style.display = "block";
+	setTimeout(function(){
+		document.getElementById("overlay").style.opacity = 1;
+	}, 10);
+	
+}
+
+function closeOverlay(){
+	document.getElementById("overlay").style.opacity = 0;
+	setTimeout(function(){
+		document.getElementById("overlay").style.display = "none";
+	}, 250);
+	
+}
+
+function display(file){
+
+	var img = document.getElementById("mainImage");
+	
+	currentlyDisplayed = file;
+	
 	var srcset = "PATH/px800/FILE 800w,\
 				  PATH/px1200/FILE 1200w,\
 				  PATH/px1600/FILE 1600w,\
@@ -10,24 +34,66 @@ function showPhoto(element){
 	var src = "PATH/px800/FILE";
 
 	srcset = srcset.replace(/PATH/g, metadata.path);
-	srcset = srcset.replace(/FILE/g, imageName);
+	srcset = srcset.replace(/FILE/g, file);
 
 	src = src.replace(/PATH/g, metadata.path);
-	src = src.replace(/FILE/g, imageName);
+	src = src.replace(/FILE/g, file);
 
-	document.getElementById("mainImage").srcset = srcset;
-	document.getElementById("mainImage").src = src;
-	document.getElementById("overlay").style.display = "block";
-	setTimeout(function(){
-		document.getElementById("overlay").style.opacity = 1;
-	}, 10);
-	
+	img.srcset = "";
+	img.src = "";
+
+	img.srcset = srcset;
+	img.src = src;
+}
+
+function next(){
+	var index = getImageIndex(currentlyDisplayed);
+	var imageCount = metadata.images.length;
+	var nextIndex = (index + 1) % imageCount;
+
+	var nextImage = metadata.images[nextIndex].file;
+	display(nextImage);
+}
+
+function previous(){
+	var index = getImageIndex(currentlyDisplayed);
+	var imageCount = metadata.images.length;
+	var previousIndex = (index - 1 + imageCount) % imageCount;
+
+	var previousImage = metadata.images[previousIndex].file;
+	display(previousImage);
 }
 
 function getImageMetadata(imageName){
 	for(var i = 0; i < metadata.images.length; i++){
-		if(images[i].file == imageName){
+		if(metadata.images[i].file == imageName){
 			return images[i];
 		}
 	}
 }
+
+function getImageIndex(imageName){
+	for(var i = 0; i < metadata.images.length; i++){
+		if(metadata.images[i].file == imageName){
+			return i;
+		}
+	}
+}
+
+window.addEventListener("keyup", function(e){
+	switch(e.key){
+		case "ArrowRight":
+		case "L":
+		case "D":
+			next();
+		break;
+		case "ArrowLeft":
+		case "H":
+		case "A":
+			previous();
+		break;
+		case "Escape":
+			closeOverlay();
+		break;
+	}
+});
